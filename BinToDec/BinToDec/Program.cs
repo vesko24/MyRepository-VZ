2d90d790dc9886319d69a1a6e47f5f08d3c720c6
vesko24 <veselin_zinkov@abv.bg>
2019-02-03 12:43:49 +0200

    BinToDec

---
+++ BinToDec/BinToDec/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Binary number: ");
+            var n = int.Parse(Console.ReadLine());
+
+            double k, p = 0, s = 0;
+
+            while (n > 0) {
+                k = Math.Pow(2, p);
+                s += (n % 10) * k;
+                p += 1;
+                n /= 10;
+            }
+
+            Console.WriteLine("Decimal number: " + s);
+        }
+    }
+}
