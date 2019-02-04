878257a0f84de8d7ad60cc61b761d3ffe8321138
vesko24 <veselin_zinkov@abv.bg>
2019-02-04 10:07:04 +0200

    DecToHex (Function)

---
+++ DecToHex (Function)/DecToHex (Function)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Decimal number: ");
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hexadecimal number: " + Convert.ToString(n, 16).ToUpper());
+        }
+    }
+}
