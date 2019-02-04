ecff5d146072bc8b829832d8302861575fcdd6e5
vesko24 <veselin_zinkov@abv.bg>
2019-02-04 11:11:08 +0200

    CircleArea

---
+++ CircleArea/CircleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("r= ");
+            var r = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Circle's area: {0:f12}", Math.PI * r * r);
+        }
+    }
+}
