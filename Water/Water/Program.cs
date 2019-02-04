196e9fbfda214d4c24ae6329f86866f58ef8e0a3
vesko24 <veselin_zinkov@abv.bg>
2019-02-04 14:00:20 +0200

    Water

---
+++ Water/Water/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n= ");
+            var n = int.Parse(Console.ReadLine());
+
+            int a, s = 0, br = 0;
+
+            for (int i = 0; i < n; i++) {
+                a = int.Parse(Console.ReadLine());
+                
+                s += a;
+
+                if (s > 255) {
+                    s -= a;
+                    br++;
+                }
+            }
+
+            for (int i = 0; i < br; i++) Console.WriteLine("Insufficient capacity!");
+            Console.WriteLine(s);
+        }
+    }
+}
