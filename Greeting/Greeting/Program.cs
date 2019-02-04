093375c5b67af34342f04d4d9b278c7c7a4db5d8
vesko24 <veselin_zinkov@abv.bg>
2019-02-04 12:50:41 +0200

    Greeting

---
+++ Greeting/Greeting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First name: ");
+            var a = Console.ReadLine();
+
+            Console.Write("Last name: ");
+            var b = Console.ReadLine();
+
+            Console.Write("Age: ");
+            var c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello, {0} {1}. You are {2} years old.", a, b, c);
+        }
+    }
+}
