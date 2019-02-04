b8ec537bd5cd46932e31b2acda34d8299971fb50
vesko24 <veselin_zinkov@abv.bg>
2019-02-04 11:55:25 +0200

    Elevator

---
+++ Elevator/Elevator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of people: ");
+            var n = int.Parse(Console.ReadLine());
+
+            Console.Write("Elevator capacity: ");
+            var p = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} courses", Math.Ceiling((double)n / p));
+        }
+    }
+}
