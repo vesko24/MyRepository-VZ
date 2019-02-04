3b0d14cf114da16a939bcdf97d653c883e7ff60b
vesko24 <veselin_zinkov@abv.bg>
2019-02-04 09:39:37 +0200

    CenToMin

---
+++ CenToMin/CenToMin/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Centuries: ");
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Years: " + n * 100);
+            Console.WriteLine("Days: " + n * 100 * 365);
+            Console.WriteLine("Hours: " + n * 100 * 365 * 24);
+            Console.WriteLine("Minutes: " + n * 100 * 365 * 24 * 60);
+        }
+    }
+}
