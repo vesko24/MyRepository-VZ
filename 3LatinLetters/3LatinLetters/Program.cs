04dee1f4bf18161570b39373adaaf6963d3e5f7b
vesko24 <veselin_zinkov@abv.bg>
2019-02-04 12:37:42 +0200

    3LatinLetters

---
+++ 3LatinLetters/3LatinLetters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n= ");
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    for (int k = 0; k < n; k++) {
+                        Console.Write((char)('a' + i));
+                        Console.Write((char)('a' + j));
+                        Console.WriteLine((char)('a' + k));
+                    }
+                }
+            }
+        }
+    }
+}
