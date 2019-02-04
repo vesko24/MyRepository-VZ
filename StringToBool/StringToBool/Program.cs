3083f64f97372494de20ae1fdb71ffc843499bd3
vesko24 <veselin_zinkov@abv.bg>
2019-02-04 12:16:40 +0200

    StringToBool

---
+++ StringToBool/StringToBool/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine();
+            bool b = Convert.ToBoolean(a);
+
+            if (b) Console.WriteLine("Yes");
+                else { Console.WriteLine("No"); }
+        }
+    }
+}
