9c4e86e9ab8fd25a05e533564f3753c67baacc7f
vesko24 <veselin_zinkov@abv.bg>
2019-02-05 14:14:55 +0200

    MiddleNumbers

---
+++ MiddleNumbers/MiddleNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            if (a.Length == 1) Console.WriteLine("{{ {0} }}", a[0]);
+            if (a.Length % 2 == 0) Console.WriteLine("{{ {0}, {1} }}", a[a.Length / 2 - 1], a[a.Length / 2]);
+            if (a.Length % 2 != 0 && a.Length != 1) Console.WriteLine("{{ {0}, {1}, {2} }}", a[a.Length / 2 - 1], a[a.Length / 2], a[a.Length / 2 + 1]);
+        }
+    }
+}
