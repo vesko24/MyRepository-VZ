2cff4c56c5e4df1c312ba7d4bf421acd5952d16c
vesko24 <veselin_zinkov@abv.bg>
2019-02-05 09:58:32 +0200

    ArrayStatistics

---
+++ ArrayStatistics/ArrayStatistics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int min = int.MaxValue, max = int.MinValue, s = 0, br = 0;
+
+            for (int i = 0; i < a.Length; i++) {
+                if (a[i] < min) min = a[i];
+                if (a[i] > max) max = a[i];
+                s += a[i];
+                br++;
+            }
+
+            Console.WriteLine("Min= " + min);
+            Console.WriteLine("Max= " + max);
+            Console.WriteLine("Sum= " + s);
+            Console.WriteLine("Average: {0:f2}", (double)s / br);
+        }
+    }
+}
