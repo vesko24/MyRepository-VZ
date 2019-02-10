1b20ad373f10571b11dbaf0cdab48cb8dab6f1af
vesko24 <veselin_zinkov@abv.bg>
2019-02-10 13:07:28 +0200

    ListSortNumbers

---
+++ ListSortNumbers/ListSortNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>();
+            string b;
+
+            b = Console.ReadLine();
+
+            a = b.Split(' ').Select(int.Parse).ToList();
+
+            a.Sort();
+
+            for (int i = 0; i < a.Count; i++) Console.Write(a[i] + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
