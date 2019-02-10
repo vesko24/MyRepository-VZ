7651377eeb03099792d881c7fe8a68ba6eb1f45e
vesko24 <veselin_zinkov@abv.bg>
2019-02-10 12:23:11 +0200

    ListReverseNumbersSum

---
+++ ListReverseNumbersSum/ListReverseNumbersSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>();
+            string b;
+            int c = 0, s = 0;
+
+            b = Console.ReadLine();
+
+            a = b.Split(' ').Select(int.Parse).ToList();
+
+            for (int i = 0; i < a.Count; i++) {
+                c = 0;
+
+                while (a[i] != 0) {
+                    c = c * 10 + a[i] % 10;
+                    a[i] /= 10;
+                }
+
+                s += c;
+            }
+
+            Console.WriteLine(s);
+        }
+    }
+}
