0a6c9ba49a75409a31d5e408ff03f264992d958e
vesko24 <veselin_zinkov@abv.bg>
2019-02-15 12:41:48 +0200

    2DimArrayAverage

---
+++ 2DimArrayAverage/2DimArrayAverage/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n= ");
+            var n = int.Parse(Console.ReadLine());
+
+            Console.Write("m= ");
+            var m = int.Parse(Console.ReadLine());
+
+            int[,] a = new int[n, m];
+            double b;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                b = 0;
+
+                for (int j = 0; j < m; j++) {
+                    Console.Write(a[i, j] + " ");
+                    b += a[i, j];
+                }
+
+                Console.Write(b / m);
+                Console.WriteLine();
+            }
+        }
+    }
+}
