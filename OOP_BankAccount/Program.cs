6395f473606566a9725f33681e414ad2cfe455c6
vesko24 <veselin_zinkov@abv.bg>
2019-04-02 11:24:39 +0300

    OOP_BankAccount

---
+++ OOP_BankAccount/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount Account = new BankAccount();
+            int a;
+            double b;
+
+            Console.Write("Enter ID: ");
+            a = int.Parse(Console.ReadLine());
+            Account.setID(a);
+
+            Console.Write("Enter balance: ");
+            b = double.Parse(Console.ReadLine());
+            Account.setBalance(b);
+
+            Account.printMenu();
+            Console.Write("Input: ");
+            a = int.Parse(Console.ReadLine());
+
+            while (a != 3)
+            {
+                if (a == 1)
+                {
+                    Console.Write("Deposit amount: ");
+                    b = double.Parse(Console.ReadLine());
+
+                    Account.deposit(b);
+                }
+
+                if (a == 2)
+                {
+                    Console.Write("Withdraw amount: ");
+                    b = double.Parse(Console.ReadLine());
+
+                    while (b > Account.getBalance())
+                    {
+                        Console.Write("Enter new withdraw amount: ");
+                        b = double.Parse(Console.ReadLine());
+                    }
+
+                    Account.withdraw(b);
+                }
+
+                Account.printMenu();
+                Console.Write("Input: ");
+                a = int.Parse(Console.ReadLine());
+            }
+
+            Account.print();
+        }
+    }
+}
