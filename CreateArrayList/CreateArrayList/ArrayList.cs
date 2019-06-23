7e667f9f6fc41f14682a4c9870378b3670eb6df1
vesko24 <47268245+vesko24@users.noreply.github.com>
2019-06-23 16:04:26 +0300

    CreateArrayList

---
+++ CreateArrayList/CreateArrayList/ArrayList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateArrayList
+{
+    class ArrayList<T>
+    {
+        private int initialCapacity = 2;
+        public int Count { get; private set; }
+        private T[] items;
+
+        public ArrayList() {
+            items = new T[initialCapacity];
+        }
+
+        public T this[int index] {
+            get {
+                if (index >= Count || index < 0) throw new ArgumentOutOfRangeException();
+                return items[index];
+            }
+
+            set {
+                if (index >= Count || index < 0) throw new ArgumentOutOfRangeException();
+                items[index] = value;
+            }
+        }
+
+        public void Add(T item) {
+            if (Count == items.Length) Resize();
+
+            items[Count++] = item;
+        }
+
+        private void Resize() {
+            T[] copy = new T[items.Length * 2];
+
+            for (int i = 0; i < items.Length; i++) copy[i] = items[i];
+
+            items = copy;
+        }
+
+        public T RemoveAt(int index) {
+            if (index >= Count || index < 0) throw new ArgumentOutOfRangeException();
+
+            T item = items[index];
+            Count--;
+            Shift(index);
+
+            if (Count <= items.Length / 4) Shrink();
+
+            return item;
+        }
+
+        private void Shift(int index) {
+            for (int i = index; i < Count; i++) {
+                items[i] = items[i + 1];
+            }
+        }
+
+        private void Shrink() {
+            T[] copy = new T[items.Length / 2];
+
+            for (int i = 0; i < items.Length; i++) {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
+    }
+}
---
+++ CreateArrayList/CreateArrayList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList<int> list = new ArrayList<int>();
+
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+
+            Console.WriteLine("Size= " + list.Count);
+
+            int t = list.RemoveAt(0);
+
+            Console.WriteLine(list[0]);
+            Console.WriteLine("Size= " + list.Count);
+        }
+    }
+}
