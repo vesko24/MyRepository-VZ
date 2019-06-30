8ba7c5ebca00810d40077f05c2b42ed6cab52482
vesko24 <47268245+vesko24@users.noreply.github.com>
2019-06-30 11:40:56 +0300

    Selection Sort

---
+++ SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10] { 25, 16, -9, 182, 53, 40, -18, 2, 84, 149 };
+
+            Console.WriteLine("Selection Sort:");
+            Console.WriteLine();
+            Console.WriteLine("Initial Array:");
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Sorted Array:");
+
+            SelectionSort.Sort(a);
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ SelectionSort/SelectionSort/SelectionSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class SelectionSort
+    {
+        public static void Sort<T>(T[] arr) where T : IComparable {
+            for (int i = 0; i < arr.Length - 1; i++) {
+                int smallest = i;
+
+                for (int j = i + 1; j < arr.Length; j++) {
+                    if (Less(arr[j], arr[smallest])) {
+                        smallest = j;
+                    }
+                }
+
+                Swap(arr, smallest, i);
+            }
+        }
+
+        private static bool Less(IComparable first, IComparable second) {
+            return first.CompareTo(second) < 0;
+        }
+
+        private static void Swap<T>(T[] arr, int from, int to) {
+            T temp = arr[from];
+            arr[from] = arr[to];
+            arr[to] = temp;
+        }
+    }
+}
+
